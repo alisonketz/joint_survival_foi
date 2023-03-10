a42672476f99ee4f022b727d3d3e127f2c833e61
Alison Ketz <aketz@wisc.edu>
2023-03-10 09:55:14 -0600

    turning on space

--- 11_run_model.R
+++ 11_run_model.R
@@ -56,21 +56,21 @@ modelcode <- nimbleCode({
                                   zero_mean = 1)
 
   #Beseg-York-Mollie-2 Spatial model
-  # phi[1:n_sect] ~ dcar_normal(sadj[1:n_adj_sp],
-  #                             sweights[1:n_adj_sp],
-  #                             snum[1:n_sect],
-  #                             1,
-  #                             zero_mean = 1) #sum to zero constraint
-  # hprec ~ dgamma(1, 1)
-  # # hsd ~ T(dnorm(1, 1), 0, 10)
-  # hprec1 <- hprec * 0.0000001
-  # sprec ~ dgamma(1, 1)  
-  # rho ~ dbeta(1, 1)# mixing parameter
-  # for (j in 1:n_sect){
-  #   hetero[j]~dnorm(0, hprec1)
-  #   space[j] <- (1 / sqrt(sprec)) * (sqrt((1 - rho)) * hetero[j] +
-  #                sqrt(rho / scale) * phi[j])
-  # }
+   phi[1:n_sect] ~ dcar_normal(sadj[1:n_adj_sp],
+                               sweights[1:n_adj_sp],
+                               snum[1:n_sect],
+                               1,
+                               zero_mean = 1) #sum to zero constraint
+   hprec ~ dgamma(1, 1)
+   # hsd ~ T(dnorm(1, 1), 0, 10)
+   hprec1 <- hprec * 0.0000001
+   sprec ~ dgamma(1, 1)  
+   rho ~ dbeta(1, 1)# mixing parameter
+   for (j in 1:n_sect){
+     hetero[j]~dnorm(0, hprec1)
+     space[j] <- (1 / sqrt(sprec)) * (sqrt((1 - rho)) * hetero[j] +
+                  sqrt(rho / scale) * phi[j])
+   }
 
   ############################################################
   ############################################################
@@ -946,9 +946,9 @@ nimConsts <- list(nT_overall = nT_overall,
                   n_agem = n_agem,
                   n_period = n_period,
                   n_adj_period = n_adj_period,
-                  # n_adj_sp = n_adj_sp,
+                  n_adj_sp = n_adj_sp,
                   n_sect = n_sect,
-                  # scale = scale,
+                  scale = scale,
                   space = rep(0, n_sect),
                   n_age_lookup_f = length(age_lookup_col_f),
                   n_age_lookup_m = length(age_lookup_col_m),
@@ -995,17 +995,17 @@ initsFun <- function()list(
                           b_period = rnorm(nknots_period) * 10^-4,
                           tau_period = runif(1, .1, 1),
                           tau_age = runif(1, .1, .4),
-                          # hprec = runif(1, .01, 1),
-                          # sprec = runif(1, 3, 4),
+                          hprec = runif(1, .01, 1),
+                          sprec = runif(1, 3, 4),
                           mprec = runif(1, 1.5, 1.7),
                           fprec = runif(1, 2.7, 4.2),
-                          # tfsd = runif(1,1,2),
-                          # tmsd = runif(1,3,4),
+                          #tfsd = runif(1,1,2),
+                          #tmsd = runif(1,3,4),
                           tmprec = runif(1, 4.2, 6.8),
                           tfprec = runif(1, 2.27, 3.44),
-                          # phi = seq(.0001, .0005, length = n_sect),
-                          # hetero = seq(.0001, .0005, length = n_sect),
-                          # rho = rbeta(1, 1, 1),
+                          phi = seq(.0001, .0005, length = n_sect),
+                          hetero = seq(.0001, .0005, length = n_sect),
+                          rho = rbeta(1, 1, 1),
                           m_period_foi = seq(-2, 2, length = n_period),
                           f_period_foi = seq(-2, 2, length = n_period),
                           m_age_foi = seq(-6, -4, length = n_agem),
@@ -1037,9 +1037,9 @@ parameters <- c(
               "period_effect_surv",
               "tau_period",
               "tau_age",
-              # "hprec",
-              # "sprec",
-             "f_age_foi",
+              "hprec",
+              "sprec",
+              "f_age_foi",
               "m_age_foi",
               "mprec",
               "fprec",
@@ -1051,10 +1051,10 @@ parameters <- c(
               # "tmsd",
               "tfprec"
               # "tfsd",
-              # "space",
-              # "hetero",
-              # "phi",
-              # "rho"
+              "space",
+              #"hetero",
+              #"phi",
+              "rho"
                )
 
 confMCMC <- configureMCMC(Rmodel,
